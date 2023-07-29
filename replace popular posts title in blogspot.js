67d973ab8614fb45f54572f969f3276ca6405c5e
CuongNgoGit <116303208+CuongNgoGit@users.noreply.github.com>
2023-07-29 17:57:11 +0700

    Update replace popular posts title in blogspot.js

--- replace popular posts title in blogspot.js
+++ replace popular posts title in blogspot.js
@@ -1,25 +1,27 @@
-  function getData5sLink(x) {window.open(&quot;https://www.data5s.net/search?q=&quot; + x.innerHTML, &quot;_blank&quot;)}
-  
+<script>
+    function getData5sLink(x) { window.open("https://www.data5s.net/search?q=" + x.innerHTML, "_blank") }
+
     function showTopFileId(e, fileId) {
 
-    const showButton = document.getElementsByClassName(&#39;showIdButton&#39;);
-    for (let i = 0; i &lt; showButton.length; i++) {
-      showButton[i].innerHTML = &#39;Get file ID&#39;;
-      document.getElementById(&#39;id&#39; + (1 + i)).style.display = &#39;none&#39;;
+        const showButton = document.getElementsByClassName("showIdButton");
+        for (let i = 0; i < showButton.length; i++) {
+            showButton[i].innerHTML = "Get file ID";
+            document.getElementById("id" + (1 + i)).style.display = "none";
+        }
+
+        const timeCounter = setInterval(counter, 1000);
+
+        let t = 16;
+
+        function counter() {
+            t -= 1;
+            e.innerHTML = "ID sẽ được lấy ra sau " + t + " giây";
+
+            if (t === 0) {
+                clearInterval(timeCounter);
+                document.getElementById(fileId).style.display = "";
+            }
+        }
+
     }
-
-    const timeCounter = setInterval(counter, 1000);
-
-    let t = 16;
-
-    function counter() {
-      t -= 1;
-      e.innerHTML = &quot;ID sẽ được lấy ra sau &quot; + t + &quot; giây&quot;;
-
-      if (t === 0) {
-        clearInterval(timeCounter);
-        document.getElementById(fileId).style.display = &quot;&quot;;
-      }
-    }
-
-  }
+</script>
