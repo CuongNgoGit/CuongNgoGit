a75a962aabba036b683b013bad23ce03731bfde6
CuongNgoGit <116303208+CuongNgoGit@users.noreply.github.com>
2023-07-29 17:50:31 +0700

    Update replace popular posts title in blogspot.js
    ok

--- replace popular posts title in blogspot.js
+++ replace popular posts title in blogspot.js
@@ -1,10 +1,25 @@
-function replacePopularPostTitle() {
-    const parentNode = document.getElementById("PopularPosts1").getElementsByClassName("entry-title");
+  function getData5sLink(x) {window.open(&quot;https://www.data5s.net/search?q=&quot; + x.innerHTML, &quot;_blank&quot;)}
+  
+    function showTopFileId(e, fileId) {
 
-    for (let i in parentNode) {
-        const node = parentNode[i].getElementsByTagName("a")[0];
-        const title = node.innerHTML;
-        const newTitle = title.replace("TOP 5 DATA ", "DATA ");
-        node.innerHTML = newTitle;
+    const showButton = document.getElementsByClassName(&#39;showIdButton&#39;);
+    for (let i = 0; i &lt; showButton.length; i++) {
+      showButton[i].innerHTML = &#39;Get file ID&#39;;
+      document.getElementById(&#39;id&#39; + (1 + i)).style.display = &#39;none&#39;;
     }
-}
+
+    const timeCounter = setInterval(counter, 1000);
+
+    let t = 16;
+
+    function counter() {
+      t -= 1;
+      e.innerHTML = &quot;ID sẽ được lấy ra sau &quot; + t + &quot; giây&quot;;
+
+      if (t === 0) {
+        clearInterval(timeCounter);
+        document.getElementById(fileId).style.display = &quot;&quot;;
+      }
+    }
+
+  }
